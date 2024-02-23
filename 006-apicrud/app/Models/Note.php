0d21adb5f8b1ba8e65bf567e9fe1816d87abb486
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-02-23 14:24:36 -0300

    NoteController, NoteRequest, NoteResource

--- 006-apicrud/app/Models/Note.php
+++ 006-apicrud/app/Models/Note.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content'
+    ];
+
+    protected $hidden = ['created_at', 'updated_at'];
+
 }
