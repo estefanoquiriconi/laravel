4d9bda68492cc6ddfb36b5a7df3f01f403b99e70
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-02-25 19:56:48 -0300

    MorphToMany (relacion polimórfica muchos a muchos)

--- 008-relationships/app/Models/Video.php
+++ 008-relationships/app/Models/Video.php
@@ -4,23 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphMany;
-use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
 
-class Post extends Model
+class Video extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
 
-    public function image() : MorphMany
-    {
-        return $this->morphMany(Image::class, 'imageable');
-    }
-
-    public function tags() : MorphToMany
+    public function tags(): MorphToMany
     {
         return $this->morphToMany(Tag::class, 'taggable');
     }
+
 }
