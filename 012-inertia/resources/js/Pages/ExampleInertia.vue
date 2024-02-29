c36fb2cae87051bd6aa07916c29f1a09dcbff6c8
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-02-29 08:29:51 -0300

    ExampleInertia

---
+++ 012-inertia/resources/js/Pages/ExampleInertia.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ counter }}</h1>
+    <button @click="increment">increment</button>
+    <NavLink :href="route('login')">Login</NavLink>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+    import NavLink from '@/Components/NavLink.vue';
+const counter = ref(0)
+const increment = () => {
+        counter.value++
+    }
+</script>
+
+<style scoped></style>
+
