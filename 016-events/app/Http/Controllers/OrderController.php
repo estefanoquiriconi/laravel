8f783b04fc6156494bcea486aa9b7d92d1574833
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-03-12 11:17:14 -0300

    Custom Commands

--- 016-events/app/Http/Controllers/OrderController.php
+++ 016-events/app/Http/Controllers/OrderController.php
@@ -5,15 +5,17 @@ namespace App\Http\Controllers;
 use App\Events\CreateOrderEvent;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class OrderController extends Controller
 {
     public function create()
     {
-        $order = Order::create([
-            'user_id' => 10,
-            'amount' => 25,
-        ]);
+        Artisan::call('make:order', ['user_id' => 75, 'amount' => 60]);
+        // $order = Order::create([
+        //     'user_id' => 10,
+        //     'amount' => 25,
+        // ]);
         // CreateOrderEvent::dispatch($order);
         return response()->json("Éxito");
     }
