37a31491e290ea4b3e253db0a9dd278b346113aa
estefanoquiriconi97 <estefanoquiriconi@gmail.com>
2024-03-18 23:48:49 -0300

    model

--- 018-csvexample/routes/web.php
+++ 018-csvexample/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\CsvController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [CsvController::class,'index'])->name('index');
+Route::post('/import', [CsvController::class,'import'])->name('import');
+Route::get('/export', [CsvController::class, 'export'])->name('export');
